d86d78b9b48299c672e615717484a1343478c723
stevenhaa <stevenhawork@hotmail.com>
2019-01-19 22:57:46 -0500

    Updates

---
+++ precache-manifest.7cf5fe735b6fcb76f5949e97f1eaefab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Shopify-Web-Engineer-Intern-2019/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "49e2372735b743487af8",
+    "url": "/Shopify-Web-Engineer-Intern-2019/static/css/main.021ab096.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Shopify-Web-Engineer-Intern-2019/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "579b863f8e944deeea01",
+    "url": "/Shopify-Web-Engineer-Intern-2019/static/js/1.579b863f.chunk.js"
+  },
+  {
+    "revision": "c6c03a546134c55498fb",
+    "url": "/Shopify-Web-Engineer-Intern-2019/static/js/runtime~main.c6c03a54.js"
+  },
+  {
+    "revision": "49e2372735b743487af8",
+    "url": "/Shopify-Web-Engineer-Intern-2019/static/js/main.49e23727.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Shopify-Web-Engineer-Intern-2019/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Shopify-Web-Engineer-Intern-2019/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Shopify-Web-Engineer-Intern-2019/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "579b863f8e944deeea01",
+    "url": "/Shopify-Web-Engineer-Intern-2019/static/css/1.58730987.chunk.css"
+  },
+  {
+    "revision": "a75fe2279a24f96506947e5ccc693d6c",
+    "url": "/Shopify-Web-Engineer-Intern-2019/index.html"
+  }
+];
